1b36f5c0aba456c5ab49f810523d3da925517182
Antoine <moineau.antoine@gmail.com>
2019-01-14 15:03:33 +0100

    Serializer OK

--- XML/XML_Parser/Assets/Scripts/XML_Serializer.cs
+++ XML/XML_Parser/Assets/Scripts/XML_Serializer.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System;  
 
 public class XML_Serializer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        var serializer = new XmlSerializer(typeof(Xml));
+        var stream = File.Open("Assets/Scenarios/test.xml", FileMode.Open);
+        var container = (Xml)serializer.Deserialize(stream);
+        Debug.Log(container.Questions[0].Intitule + "\n");
+        stream.Close();
+    }
 	
 	// Update is called once per frame
 	void Update () {
