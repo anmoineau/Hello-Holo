7ea6791e8ed7fa334db9891474343c884a23a8d8
Antoine <moineau.antoine@gmail.com>
2019-01-21 15:19:46 +0100

    Création d'interface améliorée

--- XML/XML_Parser/Assets/Scripts/InterfaceCreator.cs
+++ XML/XML_Parser/Assets/Scripts/InterfaceCreator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
@@ -11,27 +12,43 @@ public class InterfaceCreator : MonoBehaviour
     private List<GameObject> Buttons = new List<GameObject>();
     private Text intitule;
     private Xml scenario;
+    private int buttonWidth = 350;
+    private int buttonHeight = 40;
 
     public void Launch(Xml scenario)
     {
         this.scenario = scenario;
         intitule = this.GetComponentInChildren<Text>();
         intitule.text = scenario.Accueil.Texte;
-        int i = 0;
-        
-        foreach (int choix in scenario.Accueil.Suivants)
+        SetInterface(scenario.Accueil.Suivants);
+    }
+
+    void SetInterface(List<int> IDs, Boolean isPrecedent = false, int posStart = 0)
+    {
+        if(IDs.Count < 8)
         {
-            Buttons.Add(CreateButton(scenario.Questions[choix], new Vector3(0, 40 - (i++ * 40), 0), false));
+            int i = posStart;
+            foreach (int ID in IDs)
+            {
+                Question newQuestion = scenario.Questions.First(q => q.Id == ID);
+                float newHeight = intitule.rectTransform.localPosition[1] - buttonHeight * 1.5f - (i++ * buttonHeight * 1.1f);
+                Vector3 newPosition = new Vector3(0, newHeight, 0);
+                Buttons.Add(CreateButton(newQuestion, newPosition, isPrecedent));
+            }
+        }
+        else
+        {
+            intitule.text = "Erreur format scénario";
         }
     }
 
-    GameObject CreateButton(Question question, Vector3 position, Boolean precedent)
+    GameObject CreateButton(Question question, Vector3 position, Boolean isPrecedent)
     {
         GameObject button = Instantiate(buttonPrefab, this.transform.Find("Canvas"));
-        button.GetComponentInChildren<Text>().text = precedent? "Precedent" : question.Intitule;
+        button.GetComponentInChildren<Text>().text = isPrecedent? "Precedent" : question.Intitule;
         RectTransform bTransform = button.GetComponent<RectTransform>();
         bTransform.localPosition = position;
-        bTransform.sizeDelta = new Vector2(300, 30);
+        bTransform.sizeDelta = new Vector2(buttonWidth, buttonHeight);
         Button handler = button.GetComponent<Button>();
         handler.onClick.AddListener(() => ButtonClicked(question));
         return button;
@@ -42,12 +59,12 @@ public class InterfaceCreator : MonoBehaviour
         foreach (GameObject b in Buttons)
             Destroy(b);
         intitule.text = question.Reponse;
-        int i = 0;
-        foreach (int suivant in question.Suivants)
+        SetInterface(question.Suivants);
+        if (question.Id != 0)
         {
-            Buttons.Add(CreateButton(scenario.Questions[suivant], new Vector3(0, 30 - (i++ * 40), 0), false));
+            List<int> prec = new List<int>();
+            prec.Add(question.Precedent);
+            SetInterface(prec, true, question.Suivants.Count);
         }
-        if(question.Id != 0)
-            Buttons.Add(CreateButton(scenario.Questions[question.Precedent], new Vector3(0, 30 - (i++ * 40), 0), true));
     }
 }
