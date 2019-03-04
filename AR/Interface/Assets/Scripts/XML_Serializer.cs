9a8cd959854235b015be2bd0dc0df9ae314fbf5a
Antoine <moineau.antoine@gmail.com>
2019-03-04 18:13:13 +0100

    Integration interface en AR OK

---
+++ AR/Interface/Assets/Scripts/XML_Serializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+public class XML_Serializer : MonoBehaviour {
+    InterfaceCreator IC;
+
+	void Start () {
+        var serializer = new XmlSerializer(typeof(Xml));
+        var stream = File.Open("Assets/Scenarios/Isimatic.xml", FileMode.Open);
+        var scenario = (Xml)serializer.Deserialize(stream);
+        IC = GameObject.Find("Interface").GetComponent<InterfaceCreator>();
+        IC.Launch(scenario);
+        stream.Dispose();
+    }
+}
